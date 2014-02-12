1f08b26f22954552bc1d346ad6d2c9925604fc14
halogenandtoast <halogenandtoast@gmail.com>
2014-02-11 20:37:22 -0500

    Cleanup base_formatter

--- lib/tengu/base_formatter.rb
+++ lib/tengu/base_formatter.rb
@@ -26,22 +26,38 @@ module Tengu
     end
 
     def finished(result)
+      display_header(result)
+      display_failures
+      display_pending(result)
+    end
+
+    def display_pending(result)
+      if result.pending_count > 0
+        out.puts "#{result.pending_count} pending"
+      end
+    end
+
+    def display_header(result)
       out.puts
       out.puts "Finished in %0.5f seconds" % [Time.now - @started]
       out.puts "#{display(result.total_count, "example")}, #{display(result.failure_count, "failure")}"
+    end
+
+    def display_failures
       if @failures.count > 0
         puts
         @failures.each do |it_block|
-          puts it_block.description
-          it_block.expectations.each do |expectation|
-            unless expectation.success?
-              puts expectation.message
-            end
-          end
+          display_failure(it_block)
         end
       end
-      if result.pending_count > 0
-        out.puts "#{result.pending_count} pending"
+    end
+
+    def display_failure(it_block)
+      puts it_block.description
+      it_block.expectations.each do |expectation|
+        unless expectation.success?
+          puts expectation.message
+        end
       end
     end
 
